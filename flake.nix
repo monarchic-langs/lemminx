493ed790e8a908be0b5ec43c16d22a9f8720af96
midischwarz12 <midischwarz@proton.me>
2026-07-29 21:30:02 -0500

    ci: align lemminx workflows with fleet policy

--- flake.nix
+++ flake.nix
@@ -7,10 +7,37 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    formatter.${system} = pkgs.alejandra;
-    packages.${system}.default = pkgs.lemminx;
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [pkgs.lemminx pkgs.maven pkgs.jdk];
+    formatter = {
+      ${system} = pkgs.alejandra;
+    };
+    packages = {
+      ${system}.default = pkgs.lemminx;
+    };
+    checks = {
+      ${system} = {
+        default = pkgs.lemminx;
+
+        flake-format =
+          pkgs.runCommand "lemminx-flake-format-check"
+          {nativeBuildInputs = [pkgs.alejandra];}
+          ''
+            alejandra --check ${./flake.nix}
+            touch $out
+          '';
+
+        package-metadata =
+          pkgs.runCommand "lemminx-package-metadata-check"
+          {}
+          ''
+            test "${pkgs.lib.getName pkgs.lemminx}" = "lemminx"
+            touch $out
+          '';
+      };
+    };
+    devShells = {
+      ${system}.default = pkgs.mkShell {
+        packages = [pkgs.lemminx pkgs.maven pkgs.jdk];
+      };
     };
   };
 }
